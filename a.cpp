49a20f64fd37e7114b800b4a13044d37f5cceec7
Kiranpreet <kiranpreetkaur0044@gmail.com>
2025-08-05 16:08:24 +0530

    initial commit

---
+++ a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Your code starts here
+    // Declare the variables
+    int i;
+    long long j;
+    char a;
+    float b;
+    double c;
+    
+    // take input 
+    cin >> i;
+    cin >> j;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    
+    // printing those variable for output and ending with endl
+    cout << i << endl;
+    cout << j << endl;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    
+    return 0;
+}
